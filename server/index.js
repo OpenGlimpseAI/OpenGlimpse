bcb79a555f5bb397182929542607c8bb112f0e33
Rayable <rayable11.0808@gmail.com>
2026-05-17 19:11:53 +0800

    Some ui additions(routing) and added basic node infrastructure including websockets for chat

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import Server from 'socket.io'
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server)
+const port = process.env.PORT;
+
+
