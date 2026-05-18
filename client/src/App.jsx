e91ccd0fbed5f4ec41fbad999174d7867688bab4
Rayable <rayable11.0808@gmail.com>
2026-05-18 15:29:27 +0800

    added react router and finished socket io integrations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-
+import Placeholder from './components/Placeholder';
+import Chat from './components/chat.jsx';
 import BottomNav from './components/bottomnav.jsx'
+import { Routes, Route } from "react-router-dom";
 import './App.css'
 
 export default function App() {
   return (
+      <>
       <BottomNav />
+      <Routes>
+        <Route path="/" element={<Placeholder />} />
+        <Route path="/chat" element={<Chat/>} />
+        <Route path="/camera" element={<Placeholder />} />
+      </Routes>
+      </>
   )
 };
