e2d2a3ba07727bc5cb57b1b727b06fe3c5b8f994
Ryan K <249889418+ryan-k-git@users.noreply.github.com>
2026-07-08 20:51:45 +0800

    facial recog endpoints

--- src/server/index.js
+++ src/server/index.js
@@ -5,12 +5,14 @@ require("dotenv").config({
     path: path.resolve(__dirname, "../../.env"),
 });
 const { attachChatServer } = require('./chatserver.cjs');
+const { attachFaceServer } = require('./modules/facial_recog/facialrecogserver.js');
 
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 3001;
 
 attachChatServer(server);
+attachFaceServer(app);
 
 app.get('/', (req, res) => {
     res.send('server is running');
