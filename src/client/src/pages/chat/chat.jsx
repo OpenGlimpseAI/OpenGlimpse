38a19d5e2ead8f56d718569efb44b5d0ac0b7a97
Rayable <rayable11.0808@gmail.com>
2026-07-14 18:22:03 +0800

    fixed chat imports

--- src/client/src/pages/chat/chat.jsx
+++ src/client/src/pages/chat/chat.jsx
@@ -2,8 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import ChatBubble from "./ChatBubble.jsx";
 import ChatInput from './ChatInput.jsx';
 import WifiRounded from '@mui/icons-material/WifiRounded'
-import WifiOffRoundedIcon from '@mui/icons-material/WifiOffRounded';
-import {db} from "../client-scripts/db.js";
+import WifiOffRoundedIcon from '@mui/icons-material/WifiOffRounded'
 const CHAT_SERVER_URL = import.meta.env.VITE_CHAT_SERVER_URL || 'ws://localhost:3001';
 
 export default function Chat() {
